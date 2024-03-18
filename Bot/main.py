32b7383fce8de0a407a263f5d075525d6436fa4d
Akhilesh Kashyap <149856218+Nota0515@users.noreply.github.com>
2024-03-18 06:25:24 +0000

    Added commands to use the api's

--- Bot/main.py
+++ Bot/main.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 import game
 import get_quote as q
 import gemini as gem
+import chatgpt as cht
 
 def runbot():
   intents= discord.Intents.default()
@@ -31,15 +32,19 @@ def runbot():
     description = "This command will give you a quote "
     
   )
-  async def inspire(ctx, *use ):
+  async def inspire():
     quote = q.get_quote()
     await ctx.send(quote)
-  
+
+#what is users context which get stores in :)
+
   @bot.command()
-  async def bolo(ctx,*what):
+  async def bolo(ctx,what):  
     await ctx.send(" ".join(what))
+
+
   @bot.command()
-  async def choice(ctx,*what):
+  async def choice(ctx,what):
     await ctx.send(random.choice(what))
 
   @bot.command(
@@ -63,10 +68,19 @@ def runbot():
     else:
       await ctx.send("please enter valid role")
 
+  @bot.command(
+    help = "personal chatting",
+    aliases = ['kanojo'],
+    description = "with this command you can call me anytime to hangon!! "
+    
+  )
+  async def gemini(ctx,what):
+    response = gem.generate_response(what)
+    await ctx.send(response)
+
   @bot.command()
-  async def quest(ctx, *what):
-    user = str(what)
-    response = gem.generate_response(user)
+  async def gpt(ctx, what):
+    response = cht.gpt(what)
     await ctx.send(response)
     
   bot.run(os.environ.get('TOKEN'))
